44ecfca7df3a1d7a77d4d22c9248f91d2b847ab5
joeyo991 <joeyosull@gmail.com>
2022-10-29 15:09:02 +0000

    Renamed views and added default image for products

--- store/models.py
+++ store/models.py
@@ -27,7 +27,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255, default='Unknown')
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', default='images/product-default.png')
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=4, decimal_places=2)
--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 
-def all_products(request):
+def product_all(request):
     products = Product.products.all()
     return render(request, 'store/home.html', {'products': products})
 
@@ -16,4 +16,4 @@ def category_list(request, category_slug):
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'store/products/detail.html', {'product': product})
+    return render(request, 'store/products/single.html', {'product': product})
